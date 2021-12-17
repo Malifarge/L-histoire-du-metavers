e2f1f5bbf0056b4a2931a6c01422b55fa7ca4d6f
Thomas <malifarge.thomas9@gmail.com>
2021-12-17 10:07:44 +0100

    création du site

---
+++ main.js
@@ -0,0 +1,16 @@
+// menu burger
+
+function showResponsiveMenu() {
+    var menu = document.getElementById("navburger");
+    var icon = document.getElementById("burger");
+    var root = document.getElementById("root");
+    if (menu.className === "") {
+        menu.className = "open";
+        icon.className = "open";
+        root.style.overflowY = "hidden";
+    } else {
+        menu.className = "";                    
+        icon.className = "";
+        root.style.overflowY = "";
+    }
+}
